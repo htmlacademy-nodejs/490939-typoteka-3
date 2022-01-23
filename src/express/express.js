4356495e419c6ce6d70272bcadba28b2c6e32581
Akzhar Taov <akzhar.taov@yandex.ru>
2022-01-23 20:42:05 +0300

    Create Pug templates

--- src/express/express.js
+++ src/express/express.js
@@ -2,14 +2,23 @@
 
 const DEFAULT_PORT = 8080;
 
+const path = require(`path`);
 const express = require(`express`);
 const mainRoutes = require(`./routes/main.js`);
 const myRoutes = require(`./routes/my.js`);
 const articlesRoutes = require(`./routes/articles.js`);
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, `./public`)));
+
+app.set(`views`, path.resolve(__dirname, `./templates/pages`));
+app.set(`view engine`, `pug`);
+
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
-app.listen(DEFAULT_PORT);
+app.on(`error`, (err) => console.error(err));
+
+app.listen(DEFAULT_PORT, () => console.info(`Server listen ${DEFAULT_PORT} port...`));
