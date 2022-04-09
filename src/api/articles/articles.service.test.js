d31e6141562d0d8be02b65f73dc8f3d079f4749f
Akzhar Taov <akzhar.taov@yandex.ru>
2022-04-09 16:50:00 +0300

    Move mock data from tests to separate json files

--- src/api/articles/articles.service.test.js
+++ src/api/articles/articles.service.test.js
@@ -6,48 +6,9 @@ const request = require(`supertest`);
 const ApiStorage = require(`${root}/src/api/api.storage.js`);
 const Api = require(`${root}/src/api/api.js`);
 const {HttpCode, ID_LENGTH} = require(`${root}/src/api/constants.js`);
+const articlesMocks = require(`${root}/mocks/articles.json`);
 
-const articles = [
-  {
-    "id": `K7eVOM`,
-    "title": `Что такое золотое сечение. Мнения.`,
-    "createdDate": `24.01.2022, 16:47:27`,
-    "announce": `Золотое сечение — соотношение двух величин, гармоническая пропорция. Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-    "fullText": `Он написал больше 30 хитов. Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много. Золотое сечение — соотношение двух величин, гармоническая пропорция. Вы можете достичь всего. Стоит только немного постараться и запастись книгами. Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры. Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле? Как начать действовать? Для начала просто соберитесь.`,
-    "category": [
-      `Музыка`
-    ],
-    "comments": [
-      {
-        "id": `Zxzibk`,
-        "text": `Мне кажется или я уже читал это где-то? Согласен с автором! Совсем немного... Планируете записать видосик на эту тему? Давно не пользуюсь стационарными компьютерами. Ноутбуки победили.`
-      },
-      {
-        "id": `ZOlMak`,
-        "text": `Плюсую, но слишком много буквы!`
-      }
-    ]
-  },
-  {
-    "id": `wL1r-A`,
-    "title": `Самый лучший музыкальный альбом этого года`,
-    "createdDate": `31.01.2022, 11:17:24`,
-    "announce": `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем. Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле? Достичь успеха помогут ежедневные повторения.`,
-    "fullText": `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете. Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами. Программировать не настолько сложно, как об этом говорят. Собрать камни бесконечности легко, если вы прирожденный герой. Вы можете достичь всего. Стоит только немного постараться и запастись книгами. Процессор заслуживает особого внимания.`,
-    "category": [
-      `Разное`,
-      `Без рамки`
-    ],
-    "comments": [
-      {
-        "id": `7015G9`,
-        "text": `Мне не нравится ваш стиль.`
-      }
-    ]
-  }
-];
-
-const apiStorage = new ApiStorage(articles, undefined);
+const apiStorage = new ApiStorage(articlesMocks, undefined);
 const api = new Api(apiStorage);
 
 describe(`Articles API end-points`, () => {
@@ -58,7 +19,7 @@ describe(`Articles API end-points`, () => {
       const res = await request(api.instance).get(`/api/articles`);
       expect(res.statusCode).toBe(HttpCode.OK);
       expect(res.headers[`content-type`]).toBe(`application/json; charset=utf-8`);
-      expect(res.body.length).toBe(articles.length);
+      expect(res.body.length).toBe(articlesMocks.length);
       expect(res.body[0]).toHaveProperty(`title`, `Что такое золотое сечение. Мнения.`);
       expect(res.body[1]).toHaveProperty(`id`, `wL1r-A`);
     });
@@ -116,7 +77,7 @@ describe(`Articles API end-points`, () => {
 
       res = await request(api.instance).get(`/api/articles`);
       expect(res.statusCode).toBe(HttpCode.OK);
-      expect(res.body.length).toBe(articles.length);
+      expect(res.body.length).toBe(articlesMocks.length);
     });
 
     test(`POST /api/articles/:articleId/comments`, async () => {
@@ -140,7 +101,7 @@ describe(`Articles API end-points`, () => {
 
       res = await request(api.instance).get(`/api/articles/${articleId}/comments`);
       expect(res.statusCode).toBe(HttpCode.OK);
-      expect(res.body.length).toBe(articles[0].comments.length);
+      expect(res.body.length).toBe(articlesMocks[0].comments.length);
 
       expect(res.body[res.body.length - 1]).toHaveProperty(`id`, newCommentId);
       expect(res.body[res.body.length - 1]).toHaveProperty(`text`, newCommentBody.text);
--- src/api/categories/categories.service.test.js
+++ src/api/categories/categories.service.test.js
@@ -6,10 +6,9 @@ const request = require(`supertest`);
 const ApiStorage = require(`${root}/src/api/api.storage.js`);
 const Api = require(`${root}/src/api/api.js`);
 const {HttpCode} = require(`${root}/src/api/constants.js`);
+const categoriesMocks = require(`${root}/mocks/categories.json`);
 
-const categories = [`Деревья`, `За жизнь`, `Без рамки`, `Разное`, `IT решения`, `Музыка`, `Кино`, `Программирование`, `Железо`];
-
-const apiStorage = new ApiStorage(undefined, categories);
+const apiStorage = new ApiStorage(undefined, categoriesMocks);
 const api = new Api(apiStorage);
 
 describe(`Categories API end-points`, () => {
@@ -20,7 +19,7 @@ describe(`Categories API end-points`, () => {
       const res = await request(api.instance).get(`/api/categories`);
       expect(res.statusCode).toBe(HttpCode.OK);
       expect(res.headers[`content-type`]).toBe(`application/json; charset=utf-8`);
-      expect(res.body.length).toBe(categories.length);
+      expect(res.body.length).toBe(categoriesMocks.length);
       expect(res.body[0]).toBe(`Деревья`);
       expect(res.body[res.body.length - 1]).toBe(`Железо`);
     });
